efc2535666ebe6d6a6b30cbb70f6f751537ed4b2
Elvin Cooper <ing.elvin01cooper@gmail.com>
2024-08-09 17:10:34 -0400

    insercion de productos listo!

--- app.py
+++ app.py
@@ -1,20 +1,84 @@
 import cx_Oracle
+import os
 
 
-try:
-    conexion = cx_Oracle.connect(
-        user = 'TRAIN7',
-        password = 'TRAIN7',
-        dsn = 'SID_CR1'
-    )
-except cx_Oracle.DatabaseError as err:
-    print('No fue posible establecer la conexion', err)    
-else:
-    print('La conexion fue exitosa', conexion.version)    
+# Pantalla de Bienvenida
+def menu_principal():
+    print('=============================================================')
+    print('               SISTEMA GESTOR DE INVENTARIOS                 ')
+    print('=============================================================\n')
 
-cursor = conexion.cursor()    
-query = 'CREATE TABLE INVENTARIO (ID_PRODUCTO NUMBER(4), DESCRIPCION_PRODUCTO VARCHAR2(60), CATEGORIA VARCHAR2(50), CANTIDAD NUMBER, PRECIO NUMBER)'
-cursor.execute(query)
+    print('Por favor, seleccione una opcion:\n')
 
-conexion.close()
+    print('1. Agregar un nuevo producto al inventario')
+    print('2. Consultar productos en existencia')
+    print('3. Actualizar información de un producto')
+    print('4. Eliminar un producto del inventario')
+    print('5. Generar un reporte de inventario')
+    print('6. Salir\n')
+
+
+menu_principal()
+
+# Insercion de productos
+def digitar_producto():
+    print('=============================================================')
+    print('               AGREGAR NUEVO PRODUCTO                        ')
+    print('=============================================================\n')    
+
+    codigo   = int(input('Ingrese el código del producto: '))
+    nombre   = input('Ingrese el nombre del producto:     ')
+    categoria = input('Ingrese la categoria del producto: ')
+    cantidad = int(input('Ingrese la cantidad en stock: '  ))
+    precio   = int(input('Ingrese el precio unitario: '    ))
+
+    try:
+        conexion = cx_Oracle.connect(
+            user = 'TRAIN7',
+            password = 'TRAIN7',
+            dsn = 'SID_CR1'
+        )
+    except cx_Oracle.DatabaseError as err:
+        print('No fue posible establecer la conexion', err)    
+    else:
+        print('La conexion fue exitosa', conexion.version)    
+
+    cursor = conexion.cursor()    
+    query = f"INSERT INTO INVENTARIO VALUES ({codigo}, '{nombre}', '{categoria}' , {cantidad}, {precio})\n"
+    cursor.execute(query)
+
+    conexion.commit()
+
+    # cerrar el comando y la conexion a la base de datos
+    cursor.close()
+    conexion.close()
+
+    print('Producto agregado exitosamente al inventario\n')
+
+    opcion = input('Presione cualquier tecla para regresar al menu principal: ') 
+    #print('Presione cualquier tecla para regresar al menu principal')
+    if opcion:
+        menu_principal() 
+
+    
+
+
+
+def limpiar_pantalla():
+    # Verifica si el sistema operativo es Windows
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+
+
+opcion = int(input('Opcion deseada: '))
+if opcion == 1:
+    limpiar_pantalla()
+    digitar_producto()
+elif opcion == 6:
+    exit 
+         
 
